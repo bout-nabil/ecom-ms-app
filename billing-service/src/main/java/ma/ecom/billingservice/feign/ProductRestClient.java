5f9830a3ed1bc8afc0dce62cd3fa18934c396868
Boutachrafine <boutachrafine.nabil@gmail.com>
2025-11-22 20:15:21 +0100

    fault tolerances

--- billing-service/src/main/java/ma/ecom/billingservice/feign/ProductRestClient.java
+++ billing-service/src/main/java/ma/ecom/billingservice/feign/ProductRestClient.java
@@ -1,5 +1,6 @@
 package ma.ecom.billingservice.feign;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import ma.ecom.billingservice.model.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,5 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "inventory-service")
 public interface ProductRestClient {
     @GetMapping("/products/{id}")
+    @CircuitBreaker(name = "inventory-service", fallbackMethod = "getDefaultProduct")
     Product getProduct(@PathVariable Long id);
+
+    default Product getDefaultProduct(Long id, Exception exception){
+        exception.printStackTrace();
+        return Product.builder().id(id).build();
+    }
 }
