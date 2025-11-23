059216e0a830d614fe5bb4829b72b01594e8a32a
Boutachrafine <boutachrafine.nabil@gmail.com>
2025-11-23 13:09:55 +0100

    work on test config customer

--- customer-service/src/main/java/ma/ecom/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/ma/ecom/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package ma.ecom.customerservice;
 
+import ma.ecom.customerservice.config.CustomerConfigParams;
 import ma.ecom.customerservice.entities.Customer;
 import ma.ecom.customerservice.repository.ICustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
